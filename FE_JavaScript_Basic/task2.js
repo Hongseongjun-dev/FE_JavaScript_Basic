7f8560f6014047500f2d7b4ffae73fb68e035ce7
Hongseongjun-dev <opus706@naver.com>
2023-04-02 12:22:39 +0900

    seongjun2

--- FE_JavaScript_Basic/task2.js
+++ FE_JavaScript_Basic/task2.js
@@ -1,1 +1,18 @@
 /* 2번 문제 답안을 작성해주세요. */
+function solution(num_list) {
+    var answer = [];
+    let even = 0;
+    let odd = 0;
+    
+    for (let a = 0; a < num_list.length; a++){
+        if (num_list[a] % 2 === 1){
+            even += 1;
+        }
+        else{
+            odd += 1;
+        }
+    }
+    answer = [odd, even];
+    
+    return answer;
+}
--- FE_JavaScript_Basic/task3.js
+++ FE_JavaScript_Basic/task3.js
@@ -1,1 +1,14 @@
 /* 3번 문제 답안을 작성해주세요. */
+function solution(n) {
+    var answer = 1;
+    
+    for (var i = 1 ; i <= n; i++ ){
+        answer *=i;
+        if (answer === n){
+            return i;
+        }
+        if(answer > n){
+            return i-1;
+        }
+    }
+}
